e36b564324b132f889850fe50e01e2168bbc87fa
clbokea <clbokea@gmail.com>
2019-11-20 14:50:53 +0100

    after teachings

--- src/main/java/clbo/kea/controller/IndexController.java
+++ src/main/java/clbo/kea/controller/IndexController.java
@@ -2,9 +2,11 @@ package clbo.kea.controller;
 
 import clbo.kea.model.Category;
 import clbo.kea.repositories.CategoryRepository;
+import clbo.kea.repositories.RecipeRepository;
 import clbo.kea.repositories.UnitOfMeasureRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,11 +22,25 @@ public class IndexController {
     @Autowired
     UnitOfMeasureRepository unitOfMeasureRepository;
 
-    @GetMapping("/")
-    public String index(){
+    @Autowired
+    RecipeRepository recipeRepository;
 
-        System.out.println(categoryRepository.findByDescription("American").get().getDescription());
-        System.out.println(unitOfMeasureRepository.findByDescription("Teaspoon").get().getDescription());
+
+    @GetMapping("/cook")
+    public String icook(){
+
+        System.out.println(recipeRepository.findByCookTime(10).get().getCookTime());
+
+        return "index";
+    }
+
+    @GetMapping("/")
+    public String index(Model model){
+
+        model.addAttribute("category", categoryRepository.findByDescription("American").get());
+
+       // System.out.println(categoryRepository.findByDescription("American").get().getDescription());
+       // System.out.println(unitOfMeasureRepository.findByDescription("Teaspoon").get().getDescription());
         return "index";
     }
 
--- src/main/java/clbo/kea/repositories/RecipeRepository.java
+++ src/main/java/clbo/kea/repositories/RecipeRepository.java
@@ -3,7 +3,9 @@ package clbo.kea.repositories;
 import clbo.kea.model.Recipe;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
 
-
+    Optional<Recipe> findByCookTime(int cooktime);
 }
